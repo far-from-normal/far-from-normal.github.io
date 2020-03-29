18e3871ac84a86e5901a0cef4cda4bc0a40fec92
Jason <jason.boulet@gmail.com>
2020-03-29 14:34:53 -0400

    Initial commit

---
+++ scrape_roto.py
@@ -0,0 +1,203 @@
+# import re
+from bs4 import BeautifulSoup
+from urllib.request import urlopen
+import pandas as pd
+import numpy as np
+
+def get_data(url):
+    # get data
+    soup = BeautifulSoup(urlopen(url), features="html.parser")
+    raw = soup.find_all("td")
+    data = []
+    tag = []
+    for row in raw:
+        data.append(row.get_text())
+        tag.append(row.get('data-stat'))
+
+    # stuff into super long data frame
+    long = pd.DataFrame(
+        data=np.array([data, tag]).T,
+        columns=['data', 'tag']
+    )
+
+    # reshape / pivot
+    long['idx'] = 0
+    idx = 0
+    for index, row in long.iterrows():
+        if row['tag'] == 'player':
+            idx += 1
+        long.at[index, 'idx'] = idx
+    long = long.pivot_table(
+        index='idx', 
+        columns='tag', 
+        values='data',
+        aggfunc='first',
+        fill_value=None
+    )
+    long.reset_index(inplace=True)
+    long.drop(columns=['idx', 'team_ID'], inplace=True)
+
+    # move players col to front
+    cols = list(long)
+    cols.insert(0, cols.pop(cols.index('player')))
+    long = long.loc[:, cols]
+
+    long[cols[1:]] = long[cols[1:]].apply(pd.to_numeric, errors='coerce', axis=1)
+    long['player'] = long['player'].str.strip()
+
+    return long
+
+def get_singles(df):
+    df['1B'] = df['H'] - (df['2B'] + df['3B'] + df['HR'])
+    return df
+def get_nsb(df):
+    df['NSB'] = df['SB'] - df['CS']
+    return df
+def get_obp(df):
+    df['OBP'] = (df['H'] + df['BB'] + df['HBP']) / (df['PA'])
+    # df['OBP'] = (df['H'] + df['BB'] + df['HBP']) / (df['AB'] + df['BB'] + df['HBP'] + df['SF'])
+    return df
+def get_slg(df):
+    df['SLG'] = (df['1B'] + 2*df['2B'] + 3*df['3B'] + 4*df['HR']) / df['AB']
+    return df
+def get_whip(df):
+    df['WHIP'] = (df['H'] + df['BB']) / df['IP']
+    return df
+
+def batter_stats_augment(df):
+    # R, HR, OPB, SLG, NSB
+    df = get_singles(df)
+    df = get_nsb(df)
+    return df
+
+def pitcher_stats_augment(df):
+    # R, HR, WHIP, SO, SV
+    df = get_whip(df)
+    return df
+
+def team_pitcher_stats(df):
+    # R, HR, SO, WHIP, SV
+    cols = [
+        "Team_Name",
+        'IP',
+        'R',
+        'HR',
+        'SO',
+        'H',
+        'BB',
+        'SV'
+    ]
+    df = df[cols]
+    df = df.groupby(["Team_Name"]).sum()
+    df.reset_index(inplace=True)
+    df = get_whip(df)
+    df = df[["Team_Name", 'R', 'HR', 'SO', 'WHIP', 'SV']]
+    df.columns = ["Team_Name", 'RA', 'HRA', 'SO', 'WHIP', 'SV']
+    return df
+
+def team_batter_stats(df):
+    # R, HR, OPB, SLG, NSB
+    cols = [
+        "Team_Name",
+        'PA',
+        'AB',
+        'BB',
+        'HBP',
+        'SF',
+        'R',
+        'H',
+        '1B',
+        '2B',
+        '3B',
+        'HR',
+        'NSB'
+    ]
+    df = df[cols]
+    df = df.groupby(["Team_Name"]).sum()
+    df.reset_index(inplace=True)
+    df = get_obp(df)
+    df = get_slg(df)
+    df = df[["Team_Name", 'R', 'HR', 'OBP', 'SLG', 'NSB']]
+    return df
+
+
+def assign_players_to_teams(
+    data,
+    teams,
+    player_type
+):
+    teams = teams[teams['Type'] == player_type]
+    df = teams.merge(
+        data,
+        how='left',
+        on=['player']
+        )
+    return df
+
+def combine_team_stats(b, p):
+    roto_stats = b.merge(
+        p,
+        how='inner',
+        on=['Team_Name']
+        )
+    return roto_stats
+
+def process_teams(df):
+    df = pd.melt(df , id_vars=['Type'])
+    df.columns = ['Type', 'Team_Name', 'player']
+    cols = df.select_dtypes(include=[np.object]).columns
+    df[cols] = df[cols] \
+        .apply(lambda x: x.str.normalize('NFKD') \
+        .str.encode('ascii', errors='ignore') \
+        .str.decode('utf-8'))
+    df['player'] = df['player'] \
+        .str.replace(r"\(.*\)", "") \
+        .str.strip()
+    df = df \
+        .apply(lambda x: x.str.strip() if x.dtype == "object" else x)
+    return df
+
+def get_roto_stats(df_p, df_b):
+
+    teams_p = team_pitcher_stats(df_p)
+    teams_b = team_batter_stats(df_b)
+
+    roto_stats = combine_team_stats(
+        teams_b, 
+        teams_p)
+
+    pts_cat = {
+        'R': True,
+        'HR': True,
+        'OBP': True,
+        'SLG': True,
+        'NSB': True,
+        'RA': False,	
+        'HRA': False,	
+        'SO': True,
+        'WHIP': False,
+        'SV': True,
+    }
+
+    for col in roto_stats.columns[1:]:
+        pts_col = col + '_pts'
+        order = pts_cat[col]
+        roto_stats[pts_col] = roto_stats[col].rank(
+            method='min', 
+            ascending=order).astype(np.int32)
+
+    roto_stats['Total_pts'] = roto_stats.loc[:, 'R_pts':'SV_pts'].sum(axis = 1)
+    roto_stats['Rank'] = roto_stats['Total_pts'].rank(
+        method='min', 
+        ascending=False).astype(np.int32)
+
+    cols = list(roto_stats)
+    cols.insert(1, cols.pop(cols.index('Rank')))
+    roto_stats = roto_stats.loc[:, cols]
+    roto_stats.sort_values(
+        by=['Rank'], 
+        ascending=True, 
+        inplace=True
+        )
+
+    return roto_stats
---
+++ scrape_tester.py
@@ -0,0 +1,177 @@
+from scrape_roto import *
+from pathlib import Path
+import seaborn as sns
+
+pd.options.display.float_format = '${:,.2f}'.format
+
+def hover(hover_color="#ffff99"):
+    return dict(selector="tr:hover",
+                props=[("background-color", "%s" % hover_color)])
+
+# %% |||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
+base_url = "https://www.baseball-reference.com/sim/leagues/MLB/2020-"
+url_pitcher = base_url + "pitching.shtml"
+url_batter = base_url + "batting.shtml"
+
+# %% Raw data pull
+p_data = get_data(url_pitcher)
+b_data = get_data(url_batter)
+
+# %% augment data witha few stats
+b_data = batter_stats_augment(b_data)
+
+# %% get team-players
+df_teams = pd.read_excel("Fantasy_TeamList_1.xlsx")  
+df_teams = process_teams(df_teams)
+
+
+
+# %% team pitchers / batters
+df_p = assign_players_to_teams(
+    p_data, 
+    df_teams, 
+    'Arms'
+    )
+df_b = assign_players_to_teams(
+    b_data,
+    df_teams,
+    'Bats'
+    )
+
+df_p_outout = df_p[["Team_Name", "player", 'R', 'HR', 'SO', 'whip', 'SV']]
+df_p_outout.columns = ["Team_Name", "player", 'RA', 'HRA', 'SO', 'WHIP', 'SV']
+
+df_b_outout = get_nsb(df_b)
+df_b_outout = df_b_outout[["Team_Name", "player", 'R', 'HR', 'onbase_perc', 'slugging_perc', 'NSB']]
+df_b_outout.columns = ["Team_Name", "player", 'R', 'HR', 'OBP', 'SLG', 'NSB']
+
+df_p_outout.to_csv(
+    'pitcher_by_team.csv', 
+    index=False, 
+    float_format='%.3f'
+    )
+df_b_outout.to_csv(
+    'batter_by_team.csv', 
+    index=False, 
+    float_format='%.3f'
+    )
+
+print(df_p.head(5))
+
+# %%
+
+roto_stats = get_roto_stats(df_p, df_b)
+
+# cm = sns.light_palette("seagreen", as_cmap=True)
+# styled_table = table.style.background_gradient(cmap=cm)
+# styled_table
+format_dict = {
+    'R': np.int32,
+    'HR': np.int32,
+    'OBP': np.float16,
+    'SLG': np.float16,
+    'NSB': np.int32,
+    'RA': np.int32,
+    'HRA': np.int32,
+    'SO': np.int32,
+    'WHIP': np.float16,
+    'SV': np.int32,
+}
+for k, v in format_dict.items():
+    roto_stats[k] = roto_stats[k].astype(v)
+
+# %% Header
+footer_dict = {
+    "OOTP Sim Data": "https://www.sports-reference.com/blog/2020/03/baseball-reference-simulating-2020-season-with-out-of-the-park-baseball-21/",
+    "Batting": "https://www.baseball-reference.com/sim/leagues/MLB/2020-batting.shtml",
+    "Pitching": "https://www.baseball-reference.com/sim/leagues/MLB/2020-pitching.shtml"
+}
+footer = pd.DataFrame.from_dict(footer_dict, orient='index')
+footer.columns = ['Source']
+print(footer.head(5))
+
+
+def make_clickable(val):
+    return '<a href="{}">{}</a>'.format(val,val)
+
+
+styles_footer = [
+    hover(),
+    dict(selector="caption", props=[("caption-side", "top"),
+                                    ("font-size", "10pt"),
+                                    ('font-family', 'Helvetica')]
+                                    ),
+    dict(selector="th", props=[("font-size", "8pt"),
+                                ('font-family', 'Helvetica')]
+                                ),
+]
+styled_footer = footer.style.set_table_styles(styles_footer) \
+    .format(make_clickable) \
+    .set_properties(**{'font-size': '8pt', 'font-family': 'Helvetica'}) \
+    .set_caption("About This Simulation") \
+    .set_table_attributes("style='display:inline'")
+
+# %% Table
+import datetime
+mydate = datetime.datetime.now()
+year = mydate.strftime("%Y")
+month = mydate.strftime("%B")
+day = mydate.strftime("%d")
+time = mydate.strftime("%H:%M")
+date_str = month + " " + day + ", " + year + ", " + time + " EST"
+
+cmap = cmap=sns.diverging_palette(5, 250, as_cmap=True)
+styles = [
+    hover(),
+    dict(selector="caption", props=[("caption-side", "top"),
+                                    ("font-size", "12pt"),
+                                    ('font-family', 'Helvetica')]
+                                    ),
+    dict(selector="th", props=[("font-size", "10pt"),
+                                ('font-family', 'Helvetica')]
+                                ),
+]
+
+styled_table = roto_stats.style.set_table_styles(styles) \
+    .hide_index() \
+    .set_precision(3) \
+    .background_gradient(cmap) \
+    .set_properties(**{'font-size': '10pt', 'font-family': 'Helvetica'}) \
+    .set_caption("The Mac-3-Pitch Bush League: last updated " + date_str) \
+    .set_table_attributes("style='display:inline'")
+
+
+# %% Write
+html_table = styled_table.render()
+html_footer = styled_footer.render()
+# print(html_table)
+
+with open("index.html", "w+") as file:
+    file.write(html_table)
+    file.write(html_footer)
+
+
+
+# full_path_gdrive = Path.home().joinpath(
+#     # "Documents",
+#     # "Datum",
+#     # "Projects",
+#     # "Baseball",
+#     # "bref-2020-ootp-scrape",
+#     "Dropbox",
+#     "FantasyBB",
+#     "index.html"
+# )
+
+# print(full_path_gdrive)
+
+# with open(full_path_gdrive, "w+") as file:
+#     file.write(html_table)
+#     file.write(html_footer)
+
+# roto_stats.to_csv(
+#     'roto_stats.csv', 
+#     index=False, 
+#     float_format='%.3f'
+#     )
+# print(roto_stats.head(5))
